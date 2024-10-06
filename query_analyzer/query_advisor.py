c3afb1e09c0f492cbb2475e76f9f6da9c369eb33
LuminousLynx <robert.theis@stud.uni-bamberg.de>
2024-10-06 19:51:53 +0200

    implement suggestion generator and query analysis tool

--- query_analyzer/query_advisor.py
+++ query_analyzer/query_advisor.py
@@ -13,15 +13,18 @@ from search_query.constants import Operators
 from search_query.constants import SUGGESTIONS
 from search_query.constants import YIELD
 
-class QueryAdvisor():
+# pylint: disable=line-too-long
 
+class QueryAdvisor():
+    """Class for generating suggestions based on query yields from list; includes helper methods"""
     def __init__(self) -> None:
+        '''Constructor for QueryAdvisor class'''
         self.suggestions = typing.List[str]
 
     def create_suggestions(self, yield_list: typing.List[typing.Dict]) -> typing.List[str]:
         '''Method for creating suggestions and interpreting analysis of subterm yields.'''
 
-        main_query_yield = yield_list[0]["yield"]
+        main_query_yield = self.get_yield_by_query(yield_list=yield_list, query=self.get_query_by_index(yield_list=yield_list, index=0))
 
         if YIELD.is_in_optimal_range(main_query_yield):
             self.suggestions.append(SUGGESTIONS.OK.value)
@@ -29,59 +32,121 @@ class QueryAdvisor():
         elif YIELD.is_high(main_query_yield):
             self.suggestions.append(SUGGESTIONS.LITTLE_TOO_HIGH.value)
             self.suggestions.append("\n")
-            query = self.identify_high_yield(yield_list=yield_list)
-            self.suggestions.append(query.to_string())
+            problem_area = self.identify_high_yield(yield_list=yield_list)
+            self.suggestions.append("\n-> ".join(query.to_string() for query in problem_area))
 
         elif YIELD.is_low(main_query_yield):
             self.suggestions.append(SUGGESTIONS.LITTLE_TOO_LOW.value)
             self.suggestions.append("\n")
-            query = self.identify_low_yield(yield_list=yield_list)
-            self.suggestions.append(query.to_string())
+            problem_area = self.identify_low_yield(yield_list=yield_list)
+            self.suggestions.append("\n-> ".join(query.to_string() for query in problem_area))
 
         elif YIELD.is_dynamite(main_query_yield):
             self.suggestions.append(SUGGESTIONS.TOO_HIGH.value)
-            query = self.identify_high_yield(yield_list=yield_list)
-            self.suggestions.append(query.to_string())
+            problem_area = self.identify_high_yield(yield_list=yield_list)
+            self.suggestions.append("\n-> ".join(query.to_string() for query in problem_area))
 
         elif YIELD.is_restrictive(main_query_yield):
             self.suggestions.append(SUGGESTIONS.TOO_LOW.value)
-            query = self.identify_low_yield(yield_list=yield_list)
-            self.suggestions.append(query.to_string())
+            problem_area = self.identify_low_yield(yield_list=yield_list)
+            self.suggestions.append("\n-> ".join(query.to_string() for query in problem_area))
         
         return self.suggestions
 
 
-    def identify_high_yield(self, yield_list: typing.List[typing.Dict]) -> Query:
-
+    def identify_high_yield(self, yield_list: typing.List[typing.Dict]) -> typing.List[Query]:
+        '''Method for identifying problematic areas in queries with high yield'''
+        
         problem_found = False
         root_query = self.get_query_by_index(yield_list=yield_list, index=0)
+        problem_area = typing.List[Query]
 
         while not problem_found:
 
+            problem_area.append(root_query)
+            if len(problem_area) > 3:
+                problem_area.pop(0)
+
             if root_query.operator:
                 children_list = self.create_children_list(yield_list=yield_list, query=root_query)
-
-                # wenn AND: max yield child nehmen, auf kinder untersuchen
-                max_yield_child = self.get_query_with_max_yield(yield_list=children_list)
-
-                # wenn OR: schauen, ob kinder jeweils in optimaler yield range liegen. Wenn ja, root_query zurückgeben, wenn nein, kinder die drüber sind untersuchen
-
-                # wenn NOT: root-query zurückgeben und Vorschlag: entweder NOT-Teil erweitern oder vorderen Teil einschränken
-
-
-
-
-
-
+                if isinstance(AndQuery, root_query):
+                    max_yield_child = self.get_query_with_max_yield(yield_list=children_list)
+                    root_query = max_yield_child
+                elif isinstance(OrQuery, root_query):
+                    for child in children_list:
+                        if not (YIELD.is_high(child["yield"]) or YIELD.is_dynamite(child["yield"])):
+                            children_list.remove(child)
+                    if len(children_list) > 0:
+                        max_yield_child = self.get_query_with_max_yield(yield_list=children_list)
+                        root_query = max_yield_child
+                    else:
+                        problem_found = True
+                elif isinstance(NotQuery, root_query):
+                        problem_found = True
             else:
-                return root_query
+                problem_found = True
+
+        self.add_specific_suggestion(problem_area=problem_area, problem="high yield")       
+        return problem_area
     
 
-    def identify_low_yield(self, yield_list: typing.List[typing.Dict]) -> Query:
+    def identify_low_yield(self, yield_list: typing.List[typing.Dict]) -> typing.List[Query]:
+        '''Method for identifying problematic areas in queries with low yield'''
+        
+        problem_found = False
+        root_query = self.get_query_by_index(yield_list=yield_list, index=0)
+        problem_area = typing.List[Query]
 
-        # TODO: Implement
-        pass
+        while not problem_found:
 
+            problem_area.append(root_query)
+            if len(problem_area) > 3:
+                problem_area.pop(0)
+
+            if root_query.operator:
+                children_list = self.create_children_list(yield_list=yield_list, query=root_query)
+                if isinstance(AndQuery, root_query):
+                    for child in children_list:
+                        if not (YIELD.is_low(child["yield"]) or YIELD.is_restrictive(child["yield"])):
+                            children_list.remove(child)
+                    if len(children_list) > 0:
+                        min_yield_child = self.get_query_with_min_yield(yield_list=children_list)
+                        root_query = min_yield_child
+                    else:
+                        problem_found = True
+                elif isinstance(OrQuery, root_query):
+                    min_yield_child = self.get_query_with_min_yield(yield_list=children_list)
+                    root_query = min_yield_child
+                elif isinstance(NotQuery, root_query):
+                        problem_found = True
+            else:
+                problem_found = True
+
+        self.add_specific_suggestion(problem_area=problem_area, problem="low yield")       
+        return problem_area
+
+
+    def add_specific_suggestion(self, problem_area: typing.List[Query], problem: str) -> None:
+        '''Method for adding specific suggestion from constants.SUGGESTIONS based on problem area analysis'''
+        
+        if problem == "high yield":
+            if isinstance(OrQuery, problem_area[-1]):
+                self.suggestions.append(SUGGESTIONS.TOO_HIGH_ONLY_OR.value)
+            elif isinstance(NotQuery, problem_area[-1]) or problem_area.count(AndQuery) > 1:
+                self.suggestions.append(SUGGESTIONS.TOO_HIGH_SOFT_RESTRICTION.value)
+            else:
+                self.suggestions.append(SUGGESTIONS.TOO_HIGH_NO_RESTRICTION.value)
+
+        elif problem == "low yield":
+            if isinstance(AndQuery, problem_area[-1]):
+                self.suggestions.append(SUGGESTIONS.TOO_LOW_ONLY_AND.value)
+            elif isinstance(NotQuery, problem_area[-1]) or problem_area.count(OrQuery) > 1:
+                self.suggestions.append(SUGGESTIONS.TOO_LOW_SOFT_EXTENSION.value)
+            else:
+                self.suggestions.append(SUGGESTIONS.TOO_LOW_NO_EXTENSION.value)
+    
+    
+    # Helper methods
 
     def get_query_by_index(self, yield_list: typing.List[typing.Dict], index: int) -> Query:
         '''Helper method for extracting specific query from yield list'''
@@ -95,15 +160,20 @@ class QueryAdvisor():
         for item in yield_list:
             if item["query"] == query:
                 return item["yield"]
-        
         return None
     
     def get_query_with_max_yield(self, yield_list: typing.List[typing.Dict]) -> Query:
         '''Helper method for extracting query with highest yield from yield list'''
 
         max_yield = max(item["yield"] for item in yield_list)
-        query = next(item["query"] for item in yield_list if item["yield"] == max_yield)
-        
+        query = next(item["query"] for item in yield_list if item["yield"] == max_yield)   
+        return query
+    
+    def get_query_with_min_yield(self, yield_list: typing.List[typing.Dict]) -> Query:	
+        '''Helper method for extracting query with lowest yield from yield list'''
+
+        min_yield = min(item["yield"] for item in yield_list)
+        query = next(item["query"] for item in yield_list if item["yield"] == min_yield) 
         return query
     
     def create_children_list(self, yield_list: typing.List[typing.Dict], query: Query) -> typing.List[typing.Dict]:
@@ -113,5 +183,4 @@ class QueryAdvisor():
 
         for child in query.children:
             children_list.append({"query": child, "yield": self.get_yield_by_query(yield_list=yield_list, query=child)})
-
         return children_list
