9d7cf4dc668d8506cac88f9a683d9af3baef4c89
LuminousLynx <robert.theis@stud.uni-bamberg.de>
2024-10-14 22:25:13 +0200

    redesign logic to correctly estimate yields and implement NOT estimator

--- query_analyzer/yield_collector.py
+++ query_analyzer/yield_collector.py
@@ -13,9 +13,8 @@ from search_query.not_query import NotQuery
 from search_query.or_query import OrQuery
 
 from search_query.constants import PLATFORM
-from search_query.constants import Operators
 
-import random   # for testing purposes, remove in final version
+
 
 # pylint: disable=line-too-long
 
@@ -26,14 +25,14 @@ class YieldCollector():
         pass
 
 
-    def collect(self, query_list: typing.List[Query], platform: PLATFORM) -> typing.List[typing.Dict]:
+    def collect(self, query_list: typing.List[Query], platform: str) -> typing.List[typing.Dict]:
         '''Main Method that gets called from the query analyzer class. Decides on method of yield collection and runs it.'''
 
         if platform == PLATFORM.WOS.value:
             yield_list = self.collect_wos(query_list=query_list)
         elif platform == PLATFORM.PUBMED.value:
             yield_list = self.collect_pubmed(query_list=query_list)
-        elif platform == PLATFORM.EBSCO.value:
+        elif platform == PLATFORM.EBSCO.value: 
             yield_list = self.collect_ebsco(query_list=query_list)
         else:
             yield_list = self.collect_estimated(query_list=query_list)
@@ -47,8 +46,7 @@ class YieldCollector():
         yield_list = []
 
         for query in query_list:
-            # query_yield = TODO: implement yield collection for WOS
-            query_yield = random.randint(10, 10000) # default value generator for testing
+            query_yield = 10 #TODO: implement yield collection for WOS
             yield_list = self.add_to_yield_list(yield_list=yield_list, query=query, query_yield=query_yield)
 
         return yield_list
@@ -60,8 +58,7 @@ class YieldCollector():
         yield_list = []
 
         for query in query_list:
-            # query_yield = TODO: implement yield collection for PubMed
-            query_yield = random.randint(10, 10000) # default value generator for testing
+            query_yield = 10 #TODO: implement yield collection for PubMed
             yield_list = self.add_to_yield_list(yield_list=yield_list, query=query, query_yield=query_yield)
 
         return yield_list
@@ -73,8 +70,7 @@ class YieldCollector():
         yield_list = []
 
         for query in query_list:
-            # query_yield = TODO: implement yield collection for EBSCO
-            query_yield = random.randint(10, 10000) # default value generator for testing
+            query_yield = 10 #TODO: implement yield collection for EBSCO
             yield_list = self.add_to_yield_list(yield_list=yield_list, query=query, query_yield=query_yield)
 
         return yield_list
@@ -84,62 +80,107 @@ class YieldCollector():
         '''Method for collecting estimated yields for APIs that do not support boolean queries'''
         
         yield_list = typing.List[typing.Dict]
+        doi_list = typing.List[typing.Dict]
         complex_query_list = typing.List[Query]
 
         # first, collect yields of single expression queries and save complex queries for later
         for query in query_list:
             if not query.operator:
-                # query_yield = TODO: call method for trivial query of respective platform
-                query_yield = random.randint(10, 10000) # default value generator for testing
-                yield_list = self.add_to_yield_list(yield_list=yield_list, query=query, query_yield=query_yield)
+                query_results = self.get_query_results(query=query, api=api)
+                
+                # secure data quality by removing non-DOI entries
+                query_results["dois"] = self.validate_doi_list(doi_list=query_results["dois"])
+                
+                # add dois and yield to respective lists
+                yield_list = self.add_to_yield_list(yield_list=yield_list, query=query, query_yield=query_results["yield"])
+                doi_list = self.add_to_doi_list(doi_list=doi_list, query=query, dois=query_results["dois"])
             else:
                 complex_query_list.append(query)
 
-        # second, collect yields of complex queries, starting from the simplest ones, because the yield of their children is already known
+        # second, estimate yields of complex queries, starting from the simplest ones, because the yield and doi samples of their children are already known
         complex_query_list.reverse()
         for query in complex_query_list:
 
+            # check if query has already been collected and skip if it has
+            for entry in yield_list:
+                if entry["query"] == query:
+                    continue
+
             if isinstance(query, OrQuery):
-                query_yield = self.collect_or_estimated(yield_list=yield_list, query=query)
-                yield_list = self.add_to_yield_list(yield_list=yield_list, query=query, query_yield=query_yield)
+                query_results = self.collect_or_estimated(query=query, doi_list=doi_list)
 
             elif isinstance(query, AndQuery):
-                query_yield = self.collect_and_estimated(yield_list=yield_list, query=query, api=api)
-                yield_list = self.add_to_yield_list(yield_list=yield_list, query=query, query_yield=query_yield)
+                query_results = self.collect_and_estimated(yield_list=yield_list, query=query, doi_list=doi_list)
 
             elif isinstance(query, NotQuery):
-                # TODO: Implement
-                pass
+                query_results = self.collect_not_estimated(yield_list=yield_list, query=query, complex_query_list=complex_query_list, api=api)
 
-        return yield_list
+                # set yield of NOT query to 0, because it wouldn't make sense to actually collect it
+                yield_list = self.add_to_yield_list(yield_list=yield_list, query=query, query_yield=0)
+
+                # enter yield and doi sample of parent query into respective lists
+                yield_list = self.add_to_yield_list(yield_list=yield_list, query=self.get_parent_query(query=query, complex_query_list=complex_query_list), query_yield=query_results["yield"])
+                doi_list = self.add_to_doi_list(doi_list=doi_list, query=self.get_parent_query(query=query, complex_query_list=complex_query_list), dois=query_results["dois"])
+
+                # skip to next iteration
+                continue
+            
+            yield_list = self.add_to_yield_list(yield_list=yield_list, query=query, query_yield=query_results["yield"])
+            doi_list = self.add_to_doi_list(doi_list=doi_list, query=query, dois=query_results["dois"])
+
+        return yield_list.reverse()
     
 
-    def collect_or_estimated(self, yield_list: typing.List[typing.Dict], query: Query) -> int:
-        '''Method for collecting estimated yields for OR queries'''
+    def collect_or_estimated(self, query: Query, doi_list: typing.List[typing.Dict] = None) -> typing.List[typing.Dict]:
+        '''Method for collecting estimated yields and DOI samples for OR queries'''
 
-        children_yield_list = [{"query": child, "yield": self.get_yield_by_query(yield_list=yield_list, query=child)} for child in query.children]
-        return sum([child["yield"] for child in children_yield_list])
+        children_doi_list = [{"query": child, "dois": self.get_doi_by_query(doi_list=doi_list, query=child)} for child in query.children]
+        no_duplicate_sample = set([item for sublist in [child["dois"] for child in children_doi_list] for item in sublist])
+        return {"yield": len(no_duplicate_sample), "dois": no_duplicate_sample}
 
 
-    def collect_and_estimated(self, yield_list: typing.List[typing.Dict], query: Query, api: str) -> int:
-        '''Method for collecting estimated yields for AND queries'''
+    def collect_and_estimated(self, yield_list: typing.List[typing.Dict], query: Query, doi_list: typing.List[typing.Dict] = None) -> typing.List[typing.Dict]:
+        '''Method for collecting estimated yields and DOI samples for AND queries'''
 
-        children_samples = typing.List[typing.List[str]]
+        children_doi_list = [{"query": child, "dois": self.get_doi_by_query(doi_list=doi_list, query=child)} for child in query.children]
 
-        for child in query.children:
-            children_samples.extend(self.get_doi_samples(query=child, api=api))
-
-        # secure data quality by removing non-DOI entries
-        for doi in children_samples:
-            if not self.matches_doi_pattern(doi):
-                children_samples.remove(doi)
-
-        unique_dois = set(children_samples)
-        doi_frequencies = Counter(children_samples)
+        no_duplicate_sample = set(children_doi_list)
+        doi_frequencies = Counter(children_doi_list)
         shared_dois = [element for element, count in doi_frequencies.items() if count == len(query.children)]
-        shared_doi_percentage = len(shared_dois) / len(unique_dois)
+        shared_doi_percentage = len(shared_dois) / len(no_duplicate_sample)
 
-        return math.ceil(shared_doi_percentage * sum([self.get_yield_by_query(yield_list=yield_list, query=child) for child in query.children]))
+        return {"yield": math.ceil(shared_doi_percentage * sum([self.get_yield_by_query(yield_list=yield_list, query=child) for child in query.children])), "dois": shared_dois}
+
+
+    def collect_not_estimated(self, yield_list: typing.List[typing.Dict], query: Query, complex_query_list: typing.List[Query], doi_list: typing.List[typing.Dict] = None) -> typing.List[typing.Dict]:
+        '''Method for collecting estimated yields and DOI samples for queries containing NOT queries'''
+
+        # As it would not make much sense to calculate the yield of a NOT query, this method calculates the yield of the parent query and returns both doi list and yield.
+        # In the main method, both yield_list and doi_list are updated accordingly. 
+
+        parent_query = self.get_parent_query(query=query, complex_query_list=complex_query_list)
+        sibling_queries = parent_query.children.remove(query)
+
+        if isinstance(parent_query, OrQuery):
+            sibling_doi_list = [{"query": sibling, "dois": self.get_doi_by_query(doi_list=doi_list, query=sibling)} for sibling in sibling_queries]
+            sibling_sample = set([item for sublist in [sibling["dois"] for sibling in sibling_doi_list] for item in sublist])
+            query_yield = len(sibling_sample)
+        
+        elif isinstance(parent_query, AndQuery):
+            sibling_doi_list = [{"query": sibling, "dois": self.get_doi_by_query(doi_list=doi_list, query=sibling)} for sibling in sibling_queries]
+            doi_frequencies = Counter(sibling_doi_list)
+            sibling_sample = [element for element, count in doi_frequencies.items() if count == len(sibling_queries)]
+            query_yield = math.ceil(len(sibling_sample) / len(sibling_doi_list) * sum([self.get_yield_by_query(yield_list=yield_list, query=sibling) for sibling in sibling_queries]))
+            
+        this_sample = self.get_doi_by_query(doi_list=doi_list, query=query)
+
+        for doi in sibling_sample:
+            if doi in this_sample:
+                sibling_sample.remove(doi)
+
+        return {"yield": query_yield, "dois": sibling_sample}
+
+        
 
 
     # Helper methods
@@ -148,6 +189,12 @@ class YieldCollector():
         '''Helper method for adding query and its yield to the yield list'''
         
         return yield_list.append({"query": query, "yield": query_yield})
+
+
+    def add_to_doi_list(self, doi_list: typing.List[typing.Dict], query: Query, dois: typing.List[str]) -> typing.List[typing.Dict]:
+        '''Helper method for adding query and its DOIs to the DOI list'''
+
+        return doi_list.append({"query": query, "dois": dois})
     
 
     def get_yield_by_query(self, yield_list: typing.List[typing.Dict], query: Query) -> int:
@@ -157,13 +204,36 @@ class YieldCollector():
             if item["query"] == query:
                 return item["yield"]
         return None
-    
-    
-    def get_doi_samples(self, query: Query, api: str) -> typing.List[str]:
-        '''Helper method for collecting a sample of 50 DOIs to estimate complex query yield'''
 
-        # TODO: Implement: collecting DOIs and adding them to a list
+
+    def get_doi_by_query(self, doi_list: typing.List[typing.Dict], query: Query) -> typing.List[str]:
+        '''Helper method for extracting DOIs by query from DOI list'''
+
+        for item in doi_list:
+            if item["query"] == query:
+                return item["dois"]
+        return None
+
+    def get_parent_query(self, query: Query, complex_query_list: typing.List[Query]) -> Query:
+        '''Helper method for getting the parent query of a subquery '''
+
+        for complex_query in complex_query_list:
+            if query in complex_query.children:
+                return complex_query
+        return None
+    
+    
+    def get_query_results(self, query: Query, api: str) -> typing.List[str]:
+        '''Helper method for collecting a sample of DOIs to estimate complex query yield'''
+
+        # TODO: Implement: collecting at most 200 DOIs and adding them to a list, also from complex queries themselves (recursion)
         pass
+    
+
+    def validate_doi_list(self, doi_list: typing.List[str]) -> typing.List[str]:
+        '''Helper method for validating a list of DOIs'''
+
+        return [doi for doi in doi_list if self.matches_doi_pattern(doi)]
 
 
     def matches_doi_pattern(self, doi: str) -> bool:
